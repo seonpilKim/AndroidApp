14a64171c8183fd5b7ad3b620ea3c07231a05004
seonpilKim <kimsunpil123@naver.com>
2020-09-22 22:04:03 +0900

    - 검색된 아이템 없을 시, 출력 메시지 추가

--- app/src/main/java/com/example/androidapp2020/Board/LoL/lol_find.java
+++ app/src/main/java/com/example/androidapp2020/Board/LoL/lol_find.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Filter;
 import android.widget.ListView;
 import android.widget.Toast;
 
