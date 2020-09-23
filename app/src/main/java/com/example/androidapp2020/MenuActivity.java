645b0d63dd3d5cb3484efe24035031c736f78b43
qkrwlsgh1011 <69101131+qkrwlsgh1011@users.noreply.github.com>
2020-09-23 21:01:20 +0900

    수정

--- app/src/main/java/com/example/androidapp2020/MenuActivity.java
+++ app/src/main/java/com/example/androidapp2020/MenuActivity.java
@@ -2,14 +2,20 @@ package com.example.androidapp2020;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.androidapp2020.Board.LoL.League_of_Legend;
 
 public class MenuActivity extends AppCompatActivity {
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -42,5 +48,50 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private long time= 0;
+
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis() - time >= 2000){
+            time=System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+        else if(System.currentTimeMillis() - time < 2000 ){
+            ActivityCompat.finishAffinity(this);
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.btn_main:
+                intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.btn_profile:
+                // 화면전환
+                return true;
+            case R.id.btn_friend:
+                // 화면전환
+                return true;
+            case R.id.btn_setup:
+                // 화면전환
+                return true;
+            case R.id.btn_game:
+                intent= new Intent(getApplicationContext(), Game.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
